7c8e92888d1381c3f3776ca640148104a80b744c
codewesleylima <gusterlima@hotmail.com>
2026-05-24 23:41:16 -0300

    feat(ms-payments): add PaymentStatus enum

---
+++ src/main/java/com/wzzy/library/mspayments/enums/PaymentStatus.java
@@ -0,0 +1,13 @@
+package com.wzzy.library.mspayments.enums;
+
+public enum PaymentStatus {
+    PENDING,
+    PROCESSING,
+    COMPLETED,
+    FAILED,
+    CANCELLED,
+    REFUNDED,
+    DISPUTED,
+    EXPIRED,
+    AWAITING_CONFIRMATION
+}
