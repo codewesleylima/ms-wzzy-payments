e2f50809f7371c88bb0a223dfa5557b7a7643783
codewesleylima <gusterlima@hotmail.com>
2026-05-24 23:41:20 -0300

    feat(ms-payments): add TransactionStatus enum

---
+++ src/main/java/com/wzzy/library/mspayments/enums/TransactionStatus.java
@@ -0,0 +1,12 @@
+package com.wzzy.library.mspayments.enums;
+
+public enum TransactionStatus {
+    PENDING,
+    AUTHORIZED,
+    CAPTURED,
+    DECLINED,
+    FAILED,
+    CANCELLED,
+    REFUNDED,
+    CHARGEBACK
+}
